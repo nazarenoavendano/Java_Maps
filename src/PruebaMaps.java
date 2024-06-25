43f7daf6a01fc02bb9e1e0ffd4d09a04bc5c869e
Nazareno Avendaño <98565832+nazarenoavendano@users.noreply.github.com>
2024-06-25 03:01:59 -0300

    creo los recorridos

---
+++ src/PruebaMaps.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class PruebaMaps {
+    public static void main(String[] args) {
+
+        System.out.println("MIS LISTAS DE TIPO MAP");
+
+        // HashMap
+        Map<Integer, String> miHashMap = new HashMap<>();
+        miHashMap.put(11, "Angel Di Maria");
+        miHashMap.put(10, "Lionel Messi");
+        miHashMap.put(9, "Julian Alvarez");
+        System.out.println("------------------");
+        System.out.println("HashMap: " + miHashMap);
+
+        // TreeMap
+        Map<Integer, String> miTreeMap = new TreeMap<>();
+        miTreeMap.put(11, "Angel Di Maria");
+        miTreeMap.put(10, "Lionel Messi");
+        miTreeMap.put(9, "Julian Alvarez");
+        System.out.println("------------------");
+        System.out.println("TreeMap: " + miTreeMap);
+
+        // LinkedHashMap
+        Map<Integer, String> miPrimerLinked = new LinkedHashMap<>();
+        miPrimerLinked.put(11, "Angel Di Maria");
+        miPrimerLinked.put(10, "Lionel Messi");
+        miPrimerLinked.put(9, "Julian Alvarez");
+        System.out.println("------------------");
+        System.out.println("LinkedHashMap: " + miPrimerLinked);
+
+        System.out.println("------------------");
+        System.out.println(" ");
+
+        System.out.println("---RECORRER HASHMAP---");
+        //Se lo recorrió usando un FOR 🌀🌀🌀
+        for (Map.Entry<Integer, String> entry : miHashMap.entrySet()) {
+            System.out.println("Camiseta: " + entry.getKey() + ", Jugador: " + entry.getValue());
+        }
+
+        System.out.println(" ");
+
+        System.out.println("---RECORRER TREEMAP---");
+        //Se lo recorrió usando un ITERATOR 🔥🔥🔥
+        Iterator<Map.Entry<Integer, String>> it = miTreeMap.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<Integer, String> entry = it.next();
+            System.out.println("Camiseta: " + entry.getKey() + ", Jugador: " + entry.getValue());
+        }
+
+        System.out.println(" ");
+
+        System.out.println("---RECORRER LINKEDHASHMAP---");
+        //Se lo recorrió usando un FOR EACH 🌀🌀🌀
+        for (Map.Entry<Integer, String> i : miPrimerLinked.entrySet()) {
+            System.out.println("Camiseta: " + i.getKey() + ", Jugador: " + i.getValue());
+        }
+
+    }
+}
+
